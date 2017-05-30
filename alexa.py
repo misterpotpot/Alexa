a39c921164ebc66b7cc8a04adeac719620604249
jeremy <roche.jeremy93@gmail.com>
2017-05-30 13:44:42 +0200

    V4
    merge de la fonction rank et pays dans une seule fonction
    Redécoupage fonctionnel

--- alexa.py
+++ alexa.py
@@ -11,62 +11,66 @@ def load(file):
 	print "Fichier chargé\n"
 	return output
 
+def querry(url):
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request, timeout=5)
+	body = response.read()
+	return body
+
+def scrap(body, to_find, debut, fin):
+	position_debut = body.find(to_find, 0) + debut
+	position_fin = position_debut + fin
+	output = body[position_debut:position_fin]
+	output = output.replace('\n','#')
+	output = output.replace('"','#')
+	return output
+
 def is_ecommerce(body):
 	if body.find("panier", 0) > 1:
 		return 'oui'
 	return 'non'
-
-def querry(url, to_find, debut, fin, ecommerce):
-	request = urllib2.Request(url)
-	try:
-		response = urllib2.urlopen(request, timeout=5)
-		body = response.read()
-	except:
-		return '#Erreur=1'
-	try:
-		position_debut = body.find(to_find, 0) + debut
-		position_fin = position_debut + fin
-		output = body[position_debut:position_fin]
-		output = output.replace('\n','#')
-	except:
-		output = '#Erreur=2'
-	if ecommerce == 1:
-		output = output + ' ' + is_ecommerce(body)
-	return output
 
 
 while 1:
 	print "\n\n************************"
 	print "* Crawling - Alexa *"
 	print "************************"
-	print "\nChoisissez un mode :\n'ranking'\n'pays'\n'direct'\n'quit'       => Quitter le programme"
+	print "\nChoisissez un mode :\n'alexa'		=> Crawl le rank alexa et le pays principal\n'direct'	=> Crawl la langue et si le site est marchand\n'quit'		=> Quitter le programme"
 	mode = raw_input()
 
-	if mode == "ranking":
+	if mode == "alexa":
 		nom_domaine = load("input.txt")
 		for i in nom_domaine:
 			url = "http://www.alexa.com/siteinfo/" + i
-			output = querry(url, """strong class="metrics-data align-vmiddle">""", 134, 10, 0)
-			if output == "<span styl":
-				output = "NA"
-			output = output.replace(',','') 
-			output = output.replace(' ','')
-			print i + " " + output
-                
-	elif mode == "pays":
-		nom_domaine = load("input.txt")
-		for i in nom_domaine:
-			url = "http://www.alexa.com/siteinfo/" + i
-			output = querry(url, """<a href='/topsites/countries/""", 29, 2, 0)
-			print i + " " + output
+			try:
+				body = querry(url)
+				try:
+					rank = scrap(body, """strong class="metrics-data align-vmiddle">""", 134, 10)
+					rank = rank.replace(',','') 
+					rank = rank.replace(' ','')
+					if rank == "<spanstyl":
+						rank = "NA"
+					pays = scrap(body, """<a href='/topsites/countries/""", 29, 2)
+				except:
+					print i + " #Erreur=2"
+				print i + " " + rank + " " + pays
+			except:
+				print i + " #Erreur=1"
 
 	elif mode == "direct":
 		nom_domaine = load("input.txt")
 		for i in nom_domaine:
 			url = "http://" + i
-			url2 = "http://www." + i
-			output = querry(url, "lang=", 6, 2, 1)
-			print i + " " + output
+			try:
+				body = querry(url)
+				try:
+					pays = scrap(body, "lang=", 6, 2)
+				except:
+					pays = "NA"
+				ecommerce = is_ecommerce(body)
+				print i + " " + pays + " " + ecommerce
+			except:
+				print i + " #Erreur=1"
 
 	elif mode == "quit":
 		break
